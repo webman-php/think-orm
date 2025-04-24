ef774f617861b49577cf65234c3fb30bb5b95dc6
walkor <walkor@users.noreply.github.com>
2025-04-24 11:53:52 +0800

    Update Db.php

--- src/support/think/Db.php
+++ src/support/think/Db.php
@@ -13,29 +13,26 @@
 namespace support\think;
 
 use think\db\BaseQuery;
+use think\Event;
 use think\Facade;
 use Webman\ThinkOrm\DbManager;
 
 /**
  * Class Db
- * 
- * @mixin DbManager
- * 
  * 数据库操作类
- *
- * @method static BaseQuery name(string $name) 指定当前数据表名（不含前缀）
- * @method static BaseQuery table(mixed $table) 指定当前操作的数据表
+ * @mixin \think\DbManager
+ * @method static \think\DbManager name(string $name) 指定当前数据表名（不含前缀）
+ * @method static \think\DbManager table(mixed $table) 指定当前操作的数据表
  * @method static mixed transaction(callable $callback) 执行数据库事务
  * @method static void startTrans() 启动事务
  * @method static void commit() 用于非自动提交状态下面的查询提交
  * @method static void rollback() 事务回滚
  * @method static array query(string $sql, array $bind = []) 执行查询返回数据集
- * @method static BaseQuery master(bool $readMaster = true) 设置从主服务器读取数据
+ * @method static \think\DbManager master(bool $readMaster = true) 设置从主服务器读取数据
  * @method static int execute(string $sql, array $bind = []) 执行语句
- * @method static BaseQuery connect(string|null $name = null, bool $force = false) 创建/切换数据库连接查询
+ * @method static \think\DbManager connect(string|null $name = null, bool $force = false) 创建/切换数据库连接查询
  * @method static void setConfig($config) 设置配置对象
  * @method static mixed getConfig(string $name = '', $default = null) 获取配置参数
- * @method static void setCache($config) 设置缓存对象
  * @method static void setEvent(Event $event) 设置Event对象
  * @method static void event(string $event, callable $callback) 注册回调方法
  * @method static mixed trigger(string $event, $params = null, bool $once = false) 触发事件
